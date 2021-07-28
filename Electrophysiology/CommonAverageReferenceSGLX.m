c783f4f8bafada44f212610b723f7c2c8eaf06e3
Fontanini Lab <fontaninilab@gmail.com>
2021-07-28 11:54:16 -0400

    Script for running common average reference on spikeGLX data

---
+++ Electrophysiology/CommonAverageReferenceSGLX.m
@@ -0,0 +1,43 @@
+% % function CommonAverageReferenceSGLX(MouseID, SessionID, badchanID)
+% % % Perform common average reference on spikeGLX datasets using
+% % % Neuropixel-utilities toolbox https://djoshea.github.io/neuropixel-utils/
+% % %
+% % % Note: specific folder/file name structure is assumed
+% % %
+% % % Inputs:
+% % %   MouseID (string) = mouse id
+% % %   SessionID (string) = session id
+% % %   badchanID (array) = 1xN vector with bad channel IDs to be ignored for
+% % %                       CAR
+% % %
+% % % Outputs:
+% % %   Will save a new imec.ap.bin file in a new CAR subfolder
+
+rootdir = 'D:\Neuropixel Data\';
+MouseID = 'TDPWM090';
+SessionID = 'Session1';
+sep = '\';
+
+cd(['D:\Neuropixel Data\' MouseID sep MouseID '_' SessionID '_g0'];
+mkdir([MouseID '_' SessionID '_g0_CAR']);
+
+apfilepath = ['D:\Neuropixel Data\' MouseID sep MouseID '_' SessionID '_g0' sep ...
+    MouseID '_' SessionID '_g0_imec0' sep ...
+    MouseID '_' SessionID '_g0_t0.imec.ap.bin'];
+channelMapFile = 'neuropixPhase3B1_kilosortChanMap.mat';
+imec = Neuropixel.ImecDataset(apfilepath, 'channelMap', channelMapFile);
+
+imec.markBadChannels(276:385); %Mark channels to exclude 
+ %For TDPQF070 it seems like we start to see spikes on channel 293
+ %For TDPQF086 only channels up to 266 appeared for non-CAR sorting???
+ %For TDPWM090 channels up to 275 are good
+ 
+ 
+cleanedPath = ['D:\Neuropixel Data\' MouseID sep MouseID '_' SessionID '_g0' sep ...
+    MouseID '_' SessionID '_g0_CAR' sep ...
+    MouseID '_' SessionID '_g0_t0_CAR.imec.ap.bin'];
+
+extraMeta = struct();
+extraMeta.commonAverageReferenced = true;
+fnList = {@Neuropixel.DataProcessFn.commonAverageReference};
+imec = imec.saveTransformedDataset(cleanedPath, 'transformAP', fnList, 'extraMeta', extraMeta);
