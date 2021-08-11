4d6e89a3f940962aa67d7e29083e6eab27479318
F <fontaninilab@gmail.com>
2021-08-11 18:16:49 -0400

    Code for manual habituation GUI

---
+++ Behavior/t_ZaberMotor_test.m
@@ -0,0 +1,40 @@
+function t_ZaberMotor_test (motor_num,distance)
+port = serial('COM8'); % set the port
+tic
+set(port, ...
+    'BaudRate', 115200, ...
+    'DataBits', 8, ...
+    'FlowControl', 'none', ...
+    'Parity', 'none', ...
+    'StopBits', 1, ...
+    'Terminator','CR/LF');
+
+
+set(port, 'Timeout', 0.5)
+% warning off MATLAB:serial:fgetl:unsuccessfulRead %To suppress warning
+% message
+fopen(port);
+
+protocol = Zaber.AsciiProtocol(port);
+device = Zaber.AsciiDevice.initialize(protocol,motor_num); % very slow to execute
+
+
+% tic
+% % device.home();
+% % device.waitforidle();
+% toc
+
+% distance = 14; % unit in mm; 25 mm is the max distance from Home
+position = device.Units.positiontonative(distance/1000); % convert mm to m
+
+
+device.moveabsolute(position); % Tell the device to move.
+device.waitforidle(); % Wait for the move to finish.
+toc
+
+% distance = 5; % unit in mm; 25 mm is the max distance from Home
+% position = device.Units.positiontonative(distance/1000); % convert mm to m
+% device.moveabsolute(position); % Tell the device to move.
+
+fclose(port);
+
