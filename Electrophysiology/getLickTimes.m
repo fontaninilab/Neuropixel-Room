8418ae4c8c022382e18bd8c2a065e939017caf83
Fontanini Lab <fontaninilab@gmail.com>
2021-08-27 11:36:55 -0400

    Remove licks outside of sampling window

--- Electrophysiology/getLickTimes.m
+++ Electrophysiology/getLickTimes.m
@@ -1,14 +1,17 @@
-function alignedLickTimes = getLickTimes(events,fig)
+function alignedLickTimes = getLickTimes(npEvents,behaviorEvents,fig)
 % Extracts and aligns lick times to trial start times/first central lick
 %
 % INPUTS
-%  events: struct with event data (from getSpikeEventsKS.m)
+%  npEvents: struct with event data (from getSpikeEventsKS.m)
 %          .MouseID = Mouse ID
 %          .SessionID = Session ID
 %          .lickEv = struct with lick event indices
 %          .trialStartEv = trial start event indices
 %          .fsEv = nidaq sampling rate
-%   fig: (any value) optional input to produce simple lick raster
+%
+%  behaviorEvents: struct with event data from behavior acquisition computer (from extractTrialData2AFC.m)
+%
+%  fig: (any value) optional input to produce simple lick raster
 %
 % OUTPUTS
 %  alignedLickTimes: struct with aligned lick times
@@ -22,70 +25,102 @@ function alignedLickTimes = getLickTimes(events,fig)
 %                   .labels = 4x1 cell labels for rows of lick times
 
 %Convert timestamps to time (in seconds)
-trialStartTimes = events.trialStartEv./events.fsEv; 
-centralLickTimes = events.lickEv.central./events.fsEv;
-leftLickTimes = events.lickEv.left./events.fsEv;
-rightLickTimes = events.lickEv.right./events.fsEv;
+trialStartTimes = npEvents.trialStartEv./npEvents.fsEv; 
+centralLickTimes = npEvents.lickEv.central./npEvents.fsEv;
+leftLickTimes = npEvents.lickEv.left./npEvents.fsEv;
+rightLickTimes = npEvents.lickEv.right./npEvents.fsEv;
 
-centralLickTimesTrial = centralLickTimes;
-leftLickTimesTrial = leftLickTimes;
-rightLickTimesTrial = rightLickTimes;
+centralLickArray = [];
+leftLickArray = [];
+rightLickArray = [];
 
 % Pre-allocate variables
-centralLickTimesCentral = NaN(1,length(centralLickTimes));
-leftLickTimesCentral = NaN(1,length(leftLickTimes));
-rightLickTimesCentral = NaN(1,length(rightLickTimes));
-alignedLickTimes.firstLateral = NaN(5,length(trialStartTimes));
-alignedLickTimes.firstCentral = NaN(3,length(trialStartTimes));
+
+alignedLickTimes.firstLateral = NaN(5,size(behaviorEvents,2));
+alignedLickTimes.firstCentral = NaN(3,size(behaviorEvents,2));
 
 %Lick ID's (consistent with BPOD output)
 alignedLickTimes.RlickID = 2;
 alignedLickTimes.LlickID = 1;
 
 %%% For each lick align to trial start event and first central lick %%%
-for i = 1:length(trialStartTimes)
+for i = 1:size(behaviorEvents,2)
+    aCount = 0; bCount = 0; cCount = 0;
     
-    if i < length(trialStartTimes)        
-         trialIDXcentral = find(centralLickTimes >= trialStartTimes(i) & centralLickTimes < trialStartTimes(i+1)); %Find lick times for trial i   
-    else    
-        trialIDXcentral = find(centralLickTimes >= trialStartTimes(end));
-    end
+%%% Central lick times for trial i %%%
+
+    %Extract indices of central licks for trial i
+   if i < length(trialStartTimes)        
+       trialIDXcentral = find(centralLickTimes >= trialStartTimes(i) & centralLickTimes < trialStartTimes(i+1)); %Find lick times for trial i   
+   else    
+       trialIDXcentral = find(centralLickTimes >= trialStartTimes(end));
+   end
    
    if ~isempty(trialIDXcentral)
        
-       %%% Central lick times for trial i %%%
-       trialNc(trialIDXcentral) = i; %Populate vector with trial N's
-       centralLickTimesTrial(trialIDXcentral) = centralLickTimesTrial(trialIDXcentral) - trialStartTimes(i);% Align to trial start time
-       centralLickTimesCentral(trialIDXcentral) = centralLickTimesTrial(trialIDXcentral) - centralLickTimesTrial(trialIDXcentral(1)); %Align to first central lick
+       centralTrialAligned = centralLickTimes(trialIDXcentral) - trialStartTimes(i); %Align to trial start time
+             
+       %Remove licks outside of sampling window
+       c = find(centralTrialAligned > behaviorEvents(i).sampleCentralev(2) | centralTrialAligned < behaviorEvents(i).sampleCentralev(1));      
+       centralTrialAligned(c) = []; trialIDXcentral(c) = [];
+       cCount = cCount + length(c);
+       
+       trialNc = i*ones(1,length(centralTrialAligned)); %Populate vector with trial N's 
+       centralCentralAligned = centralTrialAligned - centralTrialAligned(1); %Align to first central lick
+       
+       %Concatenate trial licks to full lick array
+       centralChunk = [trialNc; centralLickTimes(trialIDXcentral); centralTrialAligned; centralCentralAligned];
+       centralLickArray = [centralLickArray centralChunk];
        
        % Extract time of first central lick for trial i
        alignedLickTimes.firstCentral(1,i) = i;
-       alignedLickTimes.firstCentral(2,i) = centralLickTimes(trialIDXcentral(1)); %Raw lick time relative to session start
-       alignedLickTimes.firstCentral(3,i) = centralLickTimesTrial(trialIDXcentral(1)); %Raw lick time relative to trial start
+       alignedLickTimes.firstCentral(2,i) = centralLickTimes(trialIDXcentral(1));
+       alignedLickTimes.firstCentral(3,i) = centralTrialAligned(1); %Raw lick time relative to trial start
         
-       %%% Left lick times for trial i %%%
+%%% Left lick times for trial i %%%
        if i < length(trialStartTimes)
            trialIDXleft = find(leftLickTimes >= trialStartTimes(i) & leftLickTimes < trialStartTimes(i+1));%Find lick times for trial i
        else
            trialIDXleft = find(leftLickTimes >= trialStartTimes(end));
        end
        
-       trialNl(trialIDXleft) = i;%Populate vector with trial N's
-       leftLickTimesTrial(trialIDXleft) = leftLickTimesTrial(trialIDXleft) - trialStartTimes(i);% Align to trial start time
-       leftLickTimesCentral(trialIDXleft) = leftLickTimesTrial(trialIDXleft) - centralLickTimesTrial(trialIDXcentral(1)); %Align to first central lick 
+       leftTrialAligned = leftLickTimes(trialIDXleft) - trialStartTimes(i);       
+       
+       %Remove licks outside of sampling window
+       a = find(leftTrialAligned > behaviorEvents(i).sampleLateralev(2) | leftTrialAligned < behaviorEvents(i).sampleLateralev(1));      
+       leftTrialAligned(a) = []; trialIDXleft(a) = [];
+       aCount = aCount + length(a);
+       
+       trialNl = i*ones(1,length(leftTrialAligned)); %Populate vector with trial N's 
+       leftCentralAligned = leftTrialAligned - centralTrialAligned(1); %Align to first central lick
+       
+       %Concatenate trial licks to full lick array
+       leftChunk = [trialNl; leftLickTimes(trialIDXleft); leftTrialAligned; leftCentralAligned];
+       leftLickArray = [leftLickArray leftChunk];
+       
 
-       %%% Right lick times for trial i %%%
+%%% Right lick times for trial i %%%
        if i < length(trialStartTimes)
            trialIDXright = find(rightLickTimes >= trialStartTimes(i) & rightLickTimes < trialStartTimes(i+1));%Find lick times for trial i
        else
            trialIDXright = find(rightLickTimes >= trialStartTimes(end));
        end
        
-       trialNr(trialIDXright) = i;%Populate vector with trial N's
-       rightLickTimesTrial(trialIDXright) = rightLickTimesTrial(trialIDXright) - trialStartTimes(i);% Align to trial start time
-       rightLickTimesCentral(trialIDXright) = rightLickTimesTrial(trialIDXright) - centralLickTimesTrial(trialIDXcentral(1)); %Align to first central lick 
+       rightTrialAligned = rightLickTimes(trialIDXright) - trialStartTimes(i);       
+       
+       %Remove licks outside of sampling window
+       b = find(rightTrialAligned > behaviorEvents(i).sampleLateralev(2) | rightTrialAligned < behaviorEvents(i).sampleLateralev(1));      
+       rightTrialAligned(b) = []; trialIDXright(b) = [];
+       bCount = bCount + length(b);
+       
+       trialNr = i*ones(1,length(rightTrialAligned)); %Populate vector with trial N's 
+       rightCentralAligned = rightTrialAligned - centralTrialAligned(1); %Align to first central lick
+       
+       %Concatenate trial licks to full lick array
+       rightChunk = [trialNr; rightLickTimes(trialIDXright); rightTrialAligned; rightCentralAligned];
+       rightLickArray = [rightLickArray rightChunk];
 
-       %%% Find first lateral lick per trial %%%
+%%% Find first lateral lick per trial %%%
        rightLicks = rightLickTimes(trialIDXright);
        leftLicks = leftLickTimes(trialIDXleft);
        if isempty(rightLicks) && ~isempty(leftLicks) %If only left licks...
@@ -93,8 +128,8 @@ for i = 1:length(trialStartTimes)
            lateralID = alignedLickTimes.LlickID;
            alignedLickTimes.firstLateral(1,i) = i;
            alignedLickTimes.firstLateral(2,i) = leftLicks(1);
-           alignedLickTimes.firstLateral(3,i) = leftLickTimes(trialIDXleft(1)) - trialStartTimes(i);
-           alignedLickTimes.firstLateral(4,i) = leftLickTimesTrial(trialIDXleft(1)) - centralLickTimesTrial(trialIDXcentral(1));
+           alignedLickTimes.firstLateral(3,i) = leftTrialAligned(1);
+           alignedLickTimes.firstLateral(4,i) = leftCentralAligned(1);
            alignedLickTimes.firstLateral(5,i) = lateralID;
 
        elseif ~isempty(rightLicks) && isempty(leftLicks) %If only right licks...
@@ -102,8 +137,8 @@ for i = 1:length(trialStartTimes)
            lateralID = alignedLickTimes.RlickID;
            alignedLickTimes.firstLateral(1,i) = i;
            alignedLickTimes.firstLateral(2,i) = rightLicks(1);
-           alignedLickTimes.firstLateral(3,i) = rightLickTimes(trialIDXright(1)) - trialStartTimes(i);
-           alignedLickTimes.firstLateral(4,i) = rightLickTimesTrial(trialIDXright(1)) - centralLickTimesTrial(trialIDXcentral(1));
+           alignedLickTimes.firstLateral(3,i) = rightTrialAligned(1);
+           alignedLickTimes.firstLateral(4,i) = rightCentralAligned(1);
            alignedLickTimes.firstLateral(5,i) = lateralID;
 
        elseif ~isempty(rightLicks) && ~isempty(leftLicks) %If both left and right licks ...
@@ -113,7 +148,7 @@ for i = 1:length(trialStartTimes)
            alignedLickTimes.firstLateral(1,i) = i;
            alignedLickTimes.firstLateral(2,i) = firstlatlick;
            alignedLickTimes.firstLateral(3,i) = firstlatlick - trialStartTimes(i);
-           alignedLickTimes.firstLateral(4,i) = firstlatlick - centralLickTimesTrial(trialIDXcentral(1));
+           alignedLickTimes.firstLateral(4,i) = firstlatlick - centralTrialAligned(1);
            alignedLickTimes.firstLateral(5,i) = lateralID;
 
 
@@ -132,26 +167,18 @@ end
 
 %%% Assign lick times to output structure %%%
 
-alignedLickTimes.central(1,:) = trialNc;
-alignedLickTimes.central(2,:) = centralLickTimes;
-alignedLickTimes.central(3,:) = centralLickTimesTrial;
-alignedLickTimes.central(4,:) = centralLickTimesCentral;
+alignedLickTimes.central = centralLickArray;
+alignedLickTimes.left = leftLickArray;
+alignedLickTimes.right = rightLickArray;
 
-alignedLickTimes.left(1,:) = trialNl;
-alignedLickTimes.left(2,:) = leftLickTimes;
-alignedLickTimes.left(3,:) = leftLickTimesTrial;
-alignedLickTimes.left(4,:) = leftLickTimesCentral;
-
-alignedLickTimes.right(1,:) = trialNr;
-alignedLickTimes.right(2,:) = rightLickTimes;
-alignedLickTimes.right(3,:) = rightLickTimesTrial;
-alignedLickTimes.right(4,:) = rightLickTimesCentral;
 
 alignedLickTimes.labels = {'trial #';'lick times, recording start';'lick times, trial start';'lick times, first central'};
 
+fprintf('Removed %d lateral licks and %d central licks\n',[aCount + bCount,cCount]);
+
 
 %%% Plot lick raster %%%
-if nargin > 1
+if nargin > 2
    
    subplot(2,1,1); % Trial-aligned lick times
     scatter(alignedLickTimes.central(3,:),alignedLickTimes.central(1,:),10,'filled','k');
@@ -175,7 +202,7 @@ if nargin > 1
     set(gcf,'PaperSize',ppsize);
     set(gcf,'Position',[0 0 ppsize]);
     
-    sgtitle([events.MouseID ' ' events.SessionID ' Lick Times'],'FontSize',20, 'Color', 'red')
+    sgtitle([npEvents.MouseID ' ' npEvents.SessionID ' Lick Times'],'FontSize',20, 'Color', 'red')
 end
 
 end
