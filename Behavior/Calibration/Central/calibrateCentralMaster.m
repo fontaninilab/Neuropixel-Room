22905eaab227170609e67a0423c7a7045bc3c8fe
F <fontaninilab@gmail.com>
2022-03-30 11:12:20 -0400

    File rename

--- Behavior/Calibration/Central/calibrateCentralMaster.m
+++ Behavior/Calibration/Central/calibrateCentralMaster.m
@@ -12,7 +12,7 @@ cd([rootdir date]);
 
 %% ---- Run next two sections for each valve ----
 
-valveN = 1; %Valve number
+valveN = 2; %Valve number
 [valveOpenTimes, nTrials] = calibrateCentralValve(valveN);
 
 fprintf('**********************************\n')
@@ -20,7 +20,7 @@ fprintf(' Calibration for valve %d complete\n',valveN)
 fprintf('**********************************\n')
 
 %%
-weights = [0.136 0.21 0.327 0.452 0.572]; %Enter weights for each valve time by hand
+weights = [0.136 0.2 0.325 0.446 0.57]; %Enter weights for each valve time by hand
 data = calcCalibrationData(weights,valveOpenTimes,nTrials);
 
 save(['Valve' num2str(valveN) '-' date],'data');
@@ -30,7 +30,7 @@ fprintf(' Conversion for valve %d complete\n',valveN)
 fprintf('**********************************\n')
 
 %% --- Calculate valve times for desired output volume ---
-rewardamt = 4; % Output volume in uL
+rewardamt = 3; % Output volume in uL
 valveTimes = getValveTimesCentral(rewardamt);
 
 
